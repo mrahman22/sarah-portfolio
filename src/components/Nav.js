c5c9b127703f86308c71f3c331d755bc9de9799d
mustabur <mustaburrahman2017@hotmail.com>
2021-03-27 13:00:49 +0000

    Nav bar component created

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Listen</li>
+                <li>Teaching</li>
+                <li>Contact</li>
+
+            </ul>
+            
+        </div>
+    )
+}
